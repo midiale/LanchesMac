f12ffac70db1962afff3ad4094abd41a7c7a8af2
Alexandre Alves <midiale@gmail.com>
2023-06-06 23:06:33 -0300

    LanchesMac

---
+++ LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.ViewModels
+{
+
+    public class LoginViewModel
+    {
+        
+        [Required(ErrorMessage = "Informe o Nome")]
+        [Display(Name ="Usuário")]
+        public string UserName { get; set; }
+
+        
+        [Required(ErrorMessage = "Informe a Senha")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
